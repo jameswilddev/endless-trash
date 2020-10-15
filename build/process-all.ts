3c1ecb4994765a0b74fb61c01b25a2068443e808
jameswilddev <jameswilddev@outlook.com>
2020-10-15 21:16:41 +0100

    Install one package at a time to prevent concurrency problems.

--- build/process-all.ts
+++ build/process-all.ts
@@ -3,12 +3,11 @@ import { processPackage } from "./process-package";
 import { writeRootReadme } from "./write-root-readme";
 
 export async function processAll(): Promise<void> {
-  await Promise.all([
-    processPackage([`endless-trash`]),
-    ...(await promises.readdir(`@endless-trash`)).map((name) =>
-      processPackage([`@endless-trash`, name])
-    ),
-  ]);
+  await processPackage([`endless-trash`]);
+
+  for (const name of await promises.readdir(`@endless-trash`)) {
+    await processPackage([`@endless-trash`, name]);
+  }
 
   await writeRootReadme();
 }
