b2421d3b3a42e6bcfcb2190c942252de84f41396
jameswilddev <jameswilddev@outlook.com>
2020-10-19 20:19:47 +0100

    Refactor.

--- @endless-trash/body-serializer/body-serializer/index.ts
+++ @endless-trash/body-serializer/body-serializer/index.ts
@@ -1,16 +1,11 @@
-import { EventHandler } from "@endless-trash/event-handler";
+import { PassthroughEventHandler } from "../../event-handler";
 import { BodySerializerInput } from "./body-serializer-input";
 import { BodySerializerOutput } from "./body-serializer-output";
 
 export { BodySerializerInput } from "./body-serializer-input";
 export { BodySerializerOutput } from "./body-serializer-output";
 
-export interface BodySerializer<T> {
-  <TInput, TOutput extends BodySerializerInput<T>>(
-    eventHandler: EventHandler<TInput, TOutput>
-  ): EventHandler<
-    TInput,
-    Pick<TOutput, Exclude<keyof TOutput, keyof BodySerializerOutput>> &
-      BodySerializerOutput
-  >;
-}
+export type BodySerializer<TParsed> = PassthroughEventHandler<
+  BodySerializerInput<TParsed>,
+  BodySerializerOutput
+>;
