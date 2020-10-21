011a616d2507c8b200d9d3df66ef05d6db112c05
jameswilddev <jameswilddev@outlook.com>
2020-10-21 19:08:17 +0100

    Add top-level exports.

---
+++ @endless-trash/prompt/index.ts
@@ -0,0 +1,14 @@
+export { BoundType } from "./bound-type";
+export {
+  CheckboxField,
+  FileField,
+  FloatField,
+  IntegerField,
+  ParagraphField,
+  StringField,
+  SubtitleField,
+  TitleField,
+  Field,
+} from "./field";
+export { Form } from "./form";
+export { Prompt } from "./prompt";
