fd95a5af3de264cb94fed110229ceea9381f3d29
jameswilddev <jameswilddev@outlook.com>
2020-11-29 08:05:35 +0000

    Add form group view.

---
+++ @endless-trash/dom-prompt-client/form-group-view/index.ts
@@ -0,0 +1,28 @@
+import { h, VDOM } from "hyperapp-cjs";
+import { formView } from "../form-view";
+import { PromptState } from "../prompt-state";
+import { State } from "../state";
+
+export function formGroupView(
+  promptState: PromptState,
+  formGroupName: string
+): VDOM<State> {
+  const formGroupState = promptState.formGroups[formGroupName];
+
+  return h(
+    `div`,
+    {
+      class: `form-group`,
+      id: formGroupState.id,
+    },
+    h(
+      `div`,
+      {
+        class: `forms`,
+      },
+      formGroupState.formGroup.forms.map((form) =>
+        formView(promptState, formGroupName, form.name)
+      )
+    )
+  );
+}
