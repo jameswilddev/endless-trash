de35cea4f551ecbabdf72e9c219b3e40412f9be3
jameswilddev <jameswilddev@outlook.com>
2020-11-29 08:40:08 +0000

    Add a prompt view.

---
+++ @endless-trash/dom-prompt-client/prompt-view/index.ts
@@ -0,0 +1,38 @@
+import { h, VDOM } from "hyperapp-cjs";
+import { formGroupView } from "../form-group-view";
+import { PromptState } from "../prompt-state";
+import { State } from "../state";
+
+export function promptView(promptState: PromptState): VDOM<State> {
+  let classNames: string[];
+
+  switch (promptState.sendState) {
+    case null:
+      classNames = [`interactive`, `prompt`];
+      break;
+
+    case `sending`:
+      classNames = [`prompt`, `being-sent`];
+      break;
+
+    case `awaitingResponse`:
+      classNames = [`prompt`, `awaiting-response`];
+      break;
+  }
+
+  return h(
+    `div`,
+    {
+      class: classNames,
+    },
+    h(
+      `div`,
+      {
+        class: `form-groups`,
+      },
+      promptState.prompt.formGroups.map((formGroup) =>
+        formGroupView(promptState, formGroup.name)
+      )
+    )
+  );
+}
